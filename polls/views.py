01eea4cd6ffe46183cf27d8c2667a4d270b6641b
makueiathiengdit <awetthon8@gmail.com>
2023-01-24 20:26:44 +0530

    second commit

--- polls/views.py
+++ polls/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
 from django.template import loader
-from .models import Question
+from .models import Question, Choice
 
 # Create your views here.
 def index(request):
@@ -11,15 +12,28 @@ def index(request):
     context = {
         "latest_question_list": latest_question_list,
     }
-    
-
     return HttpResponse(template.render(context, request ))
 
 def about(request):
     return HttpResponse("About page")
+
 def detail(request, question_id):
-    return HttpResponse("Your'e looking at question :%s " % question_id)
+       question = get_object_or_404(Question, pk=question_id)
+       return render(request, 'polls/detail.html', {'question': question})
+    
 def results(request, question_id):
-    return HttpResponse("You're looking at results for quesiton: %s" % question_id)
+    question  = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/results.html', {'question':question})
+    # return HttpResponse("You're looking at results for quesiton: %s" % question_id)
+
 def vote(request, question_id):
-    return HttpResponse("You're voting on question: %s" % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+         selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except(KeyError, Choice.DoesNotExist):
+         return render(request, 'polls:detail',{'question': question, 'error_message': "You didn't select a choice"})
+    else:
+         selected_choice.votes += 1
+         selected_choice.save()
+         return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))     
+    # return HttpResponse("You're voting on question: %s" % question_id)
