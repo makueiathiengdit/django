3ef8db1d1fb1e8a245062205c5d8db4e0c03e2ee
makueiathiengdit <awetthon8@gmail.com>
2023-01-31 13:09:21 +0530

    Added static files

--- web_test/urls.py
+++ web_test/urls.py
@@ -15,8 +15,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic.base import RedirectView
 
 urlpatterns = [
+    # redirect to polls page
+    path('', RedirectView.as_view(
+    url="/polls",
+    permanent=True,
+    )),
     path("admin/", admin.site.urls),
     path("polls/", include("polls.urls")),                 
 ]
